934776b9cd550e4842013661ef95276074355b10
Samuel Guedes <samuel.moura@credisis.com.br>
2021-09-20 15:53:34 -0400

    feat: implement module to filter odd number at a list

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,48 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list), do: filter(list)
 
-  @doc """
-  Hello world.
+  defp filter(list) do
+    filtered_list =
+      Enum.filter(list, fn item ->
+        item
+        |> parse_to_integer()
+        |> case do
+          {:ok, _number} -> true
+          {:error, _reason} -> false
+        end
+      end)
 
-  ## Examples
+    parsed_list =
+      Enum.map(filtered_list, fn value ->
+        case is_bitstring(value) do
+          true -> String.to_integer(value)
+          false -> value
+        end
+      end)
 
-      iex> ListFilter.hello()
-      :world
+    odd_list =
+      Enum.filter(parsed_list, fn item ->
+        (item / 2)
+        |> Float.to_string()
+        |> String.split(".")
+        |> Enum.at(1)
+        |> case do
+          "0" -> false
+          _ -> true
+        end
+      end)
 
-  """
-  def hello do
-    :world
+    Enum.reduce(odd_list, 0, fn _, acc -> acc + 1 end)
   end
+
+  defp parse_to_integer(item) when is_bitstring(item) do
+    try do
+      parsed_item = String.to_integer(item)
+      {:ok, parsed_item}
+    rescue
+      _ -> {:error, "item don't match with number type"}
+    end
+  end
+
+  defp parse_to_integer(item) when is_integer(item), do: {:ok, item}
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,11 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "count number odd of a list" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc", 9]
+
+      assert ListFilter.call(list) == 4
+    end
   end
 end
